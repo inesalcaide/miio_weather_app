2ef5ce71e398a1a45bee267cd5bbe503bfc35be6
Inês Alcaide Igreja <ines.igreja.96@gmail.com>
2022-11-28 13:17:55 +0000

    add project: code and docker

---
+++ server/app.py
@@ -0,0 +1,88 @@
+from pymongo import MongoClient
+from flask import Flask, request
+
+app = Flask(__name__)
+
+# Connecting to database
+CONNECTION_STRING = "mongodb://root:1234@mongodb_weather_db:27017/"
+client = MongoClient(CONNECTION_STRING)
+mydb = client["weather_db"]
+collection = mydb["weather"]
+
+
+# Get list of cities
+@app.route("/cities")
+def cities():
+    cities_list = collection.distinct("city_name")
+
+    if len(cities_list) == 0:
+        return {"error": "No cities in the database"}
+
+    return {"cities_names": cities_list}
+
+
+# Get average temperature for next 5 days for a given city
+@app.route("/temperature", methods=['GET'])
+def temperature():
+    # Validate city
+    city = request.args.get("city")
+    city = city.lower().capitalize()
+    if not city:
+        return {"error": "Missing city, parameter city required"}
+
+    pipeline = [
+        {"$match": {"city_name": city}},
+        {"$project": {
+            "_id": "$date",
+            "avg_tmp": {"$round": [{"$avg": ["$max_temp", "$min_temp"]}, 1]}}
+        },
+        {"$sort": {"_id": 1}}
+    ]
+
+    return {"temperatures": list(collection.aggregate(pipeline))}
+
+
+# Get list of 10 cities with the earliest sunrise for a given day
+@app.route("/sunrise", methods=['GET'])
+def sunrise():
+    # Validate city
+    day = request.args.get("day")
+    if not day:
+        return {"error": "Missing day, parameter day required"}
+
+    pipeline = [
+        {"$match": {"date": day}},
+        {"$project": {
+            "_id": "$city_name",
+            "sunrise": "$sunrise"
+        }},
+        {"$sort": {"sunrise": 1}},
+        {"$limit": 10}
+    ]
+
+    return {"cities": list(collection.aggregate(pipeline))}
+
+
+# Get list of 10 cities with the less speed of wind (average of the next 5 days)
+@app.route("/wind")
+def wind():
+    pipeline = [
+        {"$match": {}},
+        {"$group": {
+            "_id": "$city_name",
+            "avg_wind": {"$avg": "$wind"}}
+        },
+        {"$project": {
+            "rounded_avg_wind": {"$round": ["$avg_wind", 1]}
+        }},
+        {"$sort": {"rounded_avg_wind": 1}},
+        {"$limit": 10}
+    ]
+
+    return {"cities": list(collection.aggregate(pipeline))}
+
+
+if __name__ == '__main__':
+    app.run()
+
+
---
+++ server/load_database_data.py
@@ -0,0 +1,79 @@
+import requests
+from pymongo import MongoClient
+
+# Connect to database
+CONNECTION_STRING = "mongodb://root:1234@mongodb_weather_db:27017/"
+client = MongoClient(CONNECTION_STRING)
+mydb = client["weather_db"]
+collection = mydb["weather"]
+
+# Drop previous collections
+collection.drop()
+
+# list of districts in Portugal
+CITIES_LIST = ["Aveiro", "Beja", "Braga", "Bragança", "Castelo Branco", "Coimbra", "Évora", "Faro", "Guarda", "Leiria", "Lisboa", "Portalegre", "Porto", "Santarém", "Setúbal", "Viana do Castelo", "Vila Real", "Viseu"]
+
+
+cities_location_keys = []
+api_key = "3aFCmgPA87wAqA3AjDqtPf8ydOKOMkVo"
+
+# Lookup cities location keys in API
+for city in CITIES_LIST:
+    try:
+        url = f"http://dataservice.accuweather.com/locations/v1/cities/search?apikey={api_key}&q={city}&language=pt"
+        response = requests.get(url)
+        response.raise_for_status()
+
+        response = response.json()
+        for i in response:
+            if i["Country"]["ID"] == "PT":
+                cities_location_keys.append({city : i["Key"]})
+                break
+    except Exception as error:
+        print("ERROR")
+        print(error)
+
+# Lookup weather info for the next 5 days in API for cities location keys
+for key in cities_location_keys:
+    city_name = list(key.keys())[0]
+    count = 1
+    try:
+        location_key = key[city_name]
+
+        url = f"http://dataservice.accuweather.com/forecasts/v1/daily/5day/{location_key}?apikey={api_key}&details=true&metric=true"
+        response = requests.get(url)
+        response.raise_for_status()
+        response = response.json()
+
+
+        def transform_timestamp_to_time(ts: str):
+            x = (((ts.split("T"))[1].split("+"))[0]).split(":")
+            time = f"{x[0]}:{x[1]}"
+            return time
+
+
+        def transform_timestamp_to_date(ts: str):
+            x = ts.split("T")
+            return x[0]
+
+
+        def get_weather_info(info, city_name):
+            return {
+                "city_name": city_name,
+                "date": transform_timestamp_to_date(info["Date"]),
+                "sunrise": transform_timestamp_to_time(info["Sun"]["Rise"]),
+                "max_temp": info["Temperature"]["Maximum"]["Value"],
+                "min_temp": info["Temperature"]["Minimum"]["Value"],
+                "temp_unit": info["Temperature"]["Minimum"]["Unit"],
+                "wind": float(info["Day"]["Wind"]["Speed"]["Value"]),
+                "wind_unit": info["Day"]["Wind"]["Speed"]["Unit"]
+            }
+
+
+        for day in response["DailyForecasts"]:
+            collection.insert_one(get_weather_info(day, city_name))
+            city_name_index = collection.create_index("city_name")
+
+    except Exception as error:
+        print(error)
+
